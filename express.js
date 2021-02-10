ee4f15156888fba477eaefcb2ea04ba2b4cfd961
Jonathan <jonathan.max.annett@gmail.com>
2021-02-10 19:29:34 +1100

    added express
    usage:
    const express = require('express');
    const app = express();
    require(require.resolve('balloon-css').replace('balloon.min.css','express.js'))(app);
    ...

---
+++ express.js
@@ -0,0 +1,24 @@
+/*
+
+usage:
+const express = require('express');  
+const app = express();  
+require(require.resolve('balloon-css').replace('balloon.min.css','express.js'))(app);
+...
+
+*/ 
+
+var 
+ 
+ path = require('path'),
+ css_min_path=require.resolve('balloon-css'),
+ base_path = path.basedir(css_min_path),
+ css_path = path.join(base_path,"balloon.css");
+ module.exports = function(app){
+  app.get ("/balloon.css",function(req,res){
+     res.sendFile(css_path);
+  });
+  app.get ("/balloon.min.css",function(req,res){
+     res.sendFile(css_min_path);
+  });
+}
